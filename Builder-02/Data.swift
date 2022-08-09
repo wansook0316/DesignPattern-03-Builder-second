1ff39e15de5a9d27dbfd06a033e7624ceefaccf0
wansook0316 <wansook0316@gmail.com>
2022-08-09 18:01:54 +0900

    Create: 기본 파일들

---
+++ Builder-02/Data.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  Builder-02
+//
+//  Created by Choiwansik on 2022/08/09.
+//
+
+import Foundation
---
+++ Builder-02/Implementations/JSONBuilder.swift
@@ -0,0 +1,8 @@
+//
+//  JSONBuilder.swift
+//  Builder-02
+//
+//  Created by Choiwansik on 2022/08/09.
+//
+
+import Foundation
